1d834f73ad2000750934af8b0136392b258dea35
Bhavikom <bhavikmakvanarock@gmail.com>
2019-06-21 15:12:02 +0530

    commited on 21st June. first time

---
+++ GlobalActionBarService/app/src/main/java/com/example/android/globalactionbarservice/GlobalActionBarService.java
@@ -0,0 +1,164 @@
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.example.android.globalactionbarservice;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.GestureDescription;
+import android.graphics.Path;
+import android.graphics.PixelFormat;
+import android.media.AudioManager;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class GlobalActionBarService extends AccessibilityService {
+    FrameLayout mLayout;
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        try {
+            Log.e(" #### ", " on event fired : " + event.getPackageName());
+        }catch (Exception e) {
+            Log.e(" eeeee ", " on event fired : " +e.toString());
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+    @Override
+    protected void onServiceConnected() {
+        // Create an overlay and display the action bar
+        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mLayout = new FrameLayout(this);
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.type = WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY;
+        lp.format = PixelFormat.TRANSLUCENT;
+        lp.flags |= WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.gravity = Gravity.TOP;
+        LayoutInflater inflater = LayoutInflater.from(this);
+        inflater.inflate(R.layout.action_bar, mLayout);
+        wm.addView(mLayout, lp);
+
+        configurePowerButton();
+        configureVolumeButton();
+        configureScrollButton();
+        configureSwipeButton();
+
+
+    }
+    private void configurePowerButton() {
+        Button powerButton = (Button) mLayout.findViewById(R.id.power);
+        powerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performGlobalAction(GLOBAL_ACTION_POWER_DIALOG);
+            }
+        });
+    }
+    private void configureVolumeButton() {
+        Button volumeUpButton = (Button) mLayout.findViewById(R.id.volume_up);
+        volumeUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                        AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+
+                AccessibilityNodeInfo ani = getRootInActiveWindow();
+                if (ani != null) {
+                    getdata(ani);
+                }
+            }
+        });
+    }
+    private void configureScrollButton() {
+        Button scrollButton = (Button) mLayout.findViewById(R.id.scroll);
+        scrollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AccessibilityNodeInfo scrollable = findScrollableNode(getRootInActiveWindow());
+                if (scrollable != null) {
+                    scrollable.performAction(AccessibilityNodeInfo.AccessibilityAction.ACTION_SCROLL_FORWARD.getId());
+                }
+            }
+        });
+    }
+    private AccessibilityNodeInfo findScrollableNode(AccessibilityNodeInfo root) {
+        Deque<AccessibilityNodeInfo> deque = new ArrayDeque<>();
+        deque.add(root);
+        while (!deque.isEmpty()) {
+            AccessibilityNodeInfo node = deque.removeFirst();
+            if (node.getActionList().contains(AccessibilityNodeInfo.AccessibilityAction.ACTION_SCROLL_FORWARD)) {
+                return node;
+            }
+            for (int i = 0; i < node.getChildCount(); i++) {
+                deque.addLast(node.getChild(i));
+            }
+        }
+        return null;
+    }
+    private void configureSwipeButton() {
+        Button swipeButton = (Button) mLayout.findViewById(R.id.swipe);
+        swipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Path swipePath = new Path();
+                swipePath.moveTo(1000, 1000);
+                swipePath.lineTo(100, 1000);
+                GestureDescription.Builder gestureBuilder = new GestureDescription.Builder();
+                gestureBuilder.addStroke(new GestureDescription.StrokeDescription(swipePath, 0, 500));
+                dispatchGesture(gestureBuilder.build(), null, null);
+            }
+        });
+    }
+    private void getdata( AccessibilityNodeInfo accessibilityNodeInfo) {
+        Log.e(" #### "," get data called : "+accessibilityNodeInfo);
+        // if(accessibilityNodeInfo.getPackageName().equals("com.whatsapp")) {
+        //String fetchedResponse = "";
+        try {
+            for (int i = 0; i < accessibilityNodeInfo.getChildCount(); i++) {
+                AccessibilityNodeInfo child = accessibilityNodeInfo.getChild(i);
+                if (child != null) {
+                    if (child.getChildCount() == 0) {
+                        //  child.refresh();
+                        //if(child.getViewIdResourceName()!=null) {
+
+                        if (child.getText() != null) {
+
+                            Log.e("getdata",child.toString());
+
+                        }
+                    }
+                    getdata(child);
+                }
+            }
+        } catch (Exception exc) {
+            Log.e("ex",exc.getMessage());
+        }
+        //return fetchedResponse;
+    }
+}
